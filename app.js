3073135e86f77d5af4a37fc27d10b7cbb6a177c1
Benjamin Linehan <benjaminlinehan21@gmail.com>
2024-05-20 20:55:04 +1000

    Created basic random generator functions
    Created four random generator functions inside app.js for lowercase letters, uppercase letters, numbers & symbols.

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
-console.log('Hello World');
+// Password generation return functions 
+function returnLowerCase() {
+    const chars = "abcdefghijklmnopqrstuvwxyz";
+    const charsLength = chars.length;
+    return chars.charAt(Math.floor(Math.random() * charsLength));
+};
 
-//test
+function returnUpperCase() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const charsLength = chars.length;
+    return chars.charAt(Math.floor(Math.random() * charsLength));
+};
+
+function returnNumber() {
+    const nums = "0123456789";
+    const numsLength = nums.length;
+    return nums.charAt(Math.floor(Math.random() * numsLength));
+};
+
+function returnSymbol() {
+    const symbols = "!@#$%^&*(){}<>/,.'";
+    const symbolsLength = symbols.length;
+    return symbols.charAt(Math.floor(Math.random() * symbolsLength));
+};
+
